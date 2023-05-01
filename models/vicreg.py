dc9686de594e22c1c3572c2260fd4685394ba614
Kennedy Adriko <29119766+adken@users.noreply.github.com>
2023-05-01 21:49:14 +0200

    Update vicreg.py

--- models/vicreg.py
+++ models/vicreg.py
@@ -1,16 +1,20 @@
 import torch
-from torchvision.models import resnet50
+#from torchvision.models import resnet50
+from tempCNN import TempCNN
 import torch.nn as nn
 
 
 class VICRegNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.encoder = resnet50(pretrained=False)
-        self.encoder = torch.nn.Sequential(*(list(self.encoder.children())[:-1]),
-                                           nn.Flatten())
+        #self.encoder = resnet50(pretrained=False)
+        self.encoder = TempCNN()
+        #self.encoder = torch.nn.Sequential(*(list(self.encoder.children())[:-1]),
+        #                           nn.Flatten())
+
         self.expander = nn.Sequential(
-            nn.Linear(2048, 8192),
+            #nn.Linear(2048, 8192),
+            nn.Linear(128, 8192),
             nn.BatchNorm1d(8192),
             nn.ReLU(),
             nn.Linear(8192, 8192),
@@ -20,7 +24,10 @@ class VICRegNet(nn.Module):
 
     def forward(self, x):
         _repr = self.encoder(x)
-        _embeds = self.expander(_repr)
+        _embeds = self.expander(_repr.squeeze())
+        #_embeds = self.expander(_repr)
         return _embeds
 
 
+if __name__ == '__main__':
+    model = VICRegNet()
