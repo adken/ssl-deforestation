184a517f775f15e40187df198fe4d6673cfd3c67
Kennedy Adriko <29119766+adken@users.noreply.github.com>
2023-05-01 21:42:33 +0200

    models

---
+++ models/tempCNN.py
@@ -0,0 +1,72 @@
+import os
+import torch
+import torch.nn as nn
+import torch.utils.data
+
+"""
+Pytorch re-implementation of Pelletier et al. 2019 (https://github.com/charlotte-pel/temporalCNN) adjusted as a backbone
+encoder for self-supervised learning with VICREG 
+
+"""
+
+
+class TempCNN(torch.nn.Module):
+    def __init__(self, input_dim=13, num_classes=9, sequencelength=45, kernel_size=7, hidden_dims=128, dropout=0.18203942949809093):
+        super(TempCNN, self).__init__()
+        self.modelname = f"TempCNN_input-dim={input_dim}_num-classes={num_classes}_sequencelenght={sequencelength}_" \
+                         f"kernelsize={kernel_size}_hidden-dims={hidden_dims}_dropout={dropout}"
+
+        self.hidden_dims = hidden_dims
+
+        self.conv_bn_relu1 = Conv1D_BatchNorm_Relu_Dropout(input_dim, hidden_dims, kernel_size=kernel_size,
+                                                           drop_probability=dropout)
+        self.conv_bn_relu2 = Conv1D_BatchNorm_Relu_Dropout(hidden_dims, hidden_dims, kernel_size=kernel_size,
+                                                           drop_probability=dropout)
+        self.conv_bn_relu3 = Conv1D_BatchNorm_Relu_Dropout(hidden_dims, hidden_dims, kernel_size=kernel_size,
+                                                           drop_probability=dropout)
+        
+        # Global average pooling goes here
+        self.pool = nn.AdaptiveAvgPool1d(1)
+
+    def forward(self, x):
+        # require NxTxD
+        x = x.transpose(1,2)
+        x = self.conv_bn_relu1(x)
+        x = self.conv_bn_relu2(x)
+        x = self.conv_bn_relu3(x)
+
+        # global average pooling
+        x = self.pool(x)
+        
+        return x
+
+
+    def save(self, path="model.pth", **kwargs):
+        print("\nsaving model to " + path)
+        model_state = self.state_dict()
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        torch.save(dict(model_state=model_state, **kwargs), path)
+
+    def load(self, path):
+        print("loading model from " + path)
+        snapshot = torch.load(path, map_location="cpu")
+        model_state = snapshot.pop('model_state', snapshot)
+        self.load_state_dict(model_state)
+        return snapshot
+
+
+class Conv1D_BatchNorm_Relu_Dropout(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dims, kernel_size=5, drop_probability=0.5):
+        super(Conv1D_BatchNorm_Relu_Dropout, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv1d(input_dim, hidden_dims, kernel_size, padding=(kernel_size // 2)),
+            nn.BatchNorm1d(hidden_dims),
+            nn.ReLU(),
+            nn.Dropout(p=drop_probability)
+        )
+
+    def forward(self, X):
+        return self.block(X)
+
+
