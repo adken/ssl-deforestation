bab2c49571d9f9e5163701444b683506967dec6b
Kennedy Adriko <29119766+adken@users.noreply.github.com>
2023-05-04 17:49:48 +0200

    update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -3,23 +3,6 @@ import torch
 import math
 
 
-def augment(image):
-    transform = transforms.Compose(
-        [   transforms.RandomResizedCrop(224, scale=(0.08, 0.1)),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.ColorJitter(0.4, 0.4, 0.2, 0.1),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.RandomApply([transforms.GaussianBlur(kernel_size=23)], p=0.5),
-            transforms.RandomSolarize(threshold =128, p=0.1), #128 taken from BYOL
-            transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        ]
-    )
-    image_a = transform(image)
-    image_b = transform(image)
-    return image_a, image_b
-
-
 #function taken from https://github.com/facebookresearch/barlowtwins/
 class LARS(torch.optim.Optimizer):
     def __init__(self, params, lr, weight_decay=0, momentum=0.9, eta=0.001,
