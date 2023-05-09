593cd383c5b973f77a02c9a98afae23b077aff6d
Kennedy Adriko <29119766+adken@users.noreply.github.com>
2023-05-09 18:39:51 +0200

    update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,24 +5,23 @@ from torch.utils.data import Dataset
 
 
 
-class TimeSeriesDataset(Dataset):
+class TimeSeriesDataset(Dataset):    
     """
-    A PyTorch dataset class for loading s1 and s2 satellite time series images from a NumPy file.
-    The images are expected to have dimensions N,T x C, where:
-    H: height of images
-    W: width of images
+    A PyTorch dataset class for loading satellite time series images from a NumPy file.
+    The images are expected to have dimensions N x T x C, where:
+    N: number of samples
     T: number of timestamps in the time series
     C: number of channels in the images
     
     Parameters
     ----------
     path : str
-        The path to the directory containing the sentinel1 and sentinel2 times series data.
-      
+        The path to the NumPy file containing the image data.
+        
     Attributes
     ----------
-    s1, s2 : torch.Tensor
-        Tensors containing the image data loaded from the NumPy files.
+    x : torch.Tensor
+        A tensor containing the image data loaded from the NumPy file.
     num_samples : int
         The total number of samples (pixels) in the dataset.
     """
@@ -31,42 +30,34 @@ class TimeSeriesDataset(Dataset):
 
         self.path = path
         self.dataset1 = np.load(os.path.join(self.path, 's1_stack.npy'))
-        dataset2 = np.load(os.path.join(self.path, 's2_stack.npy'))
-        self.x2 = torch.from_numpy(dataset2)
-        self.num_samples = self.dataset1.shape[0] + self.x2.shape[0]
+        self.dataset2 = np.load(os.path.join(self.path, 's2_stack.npy'))
+        self.num_samples = self.dataset1.shape[0] + self.dataset2.shape[0]
+    
  
-
     def __len__(self):
         """
         Returns the total number of samples (pixels) in the dataset.
         """
         return self.num_samples
     
-    """ def __getitem__(self, idx):
-    
-        Returns a single sample (pixel) from the dataset as a PyTorch tensor.
-    
-        s1 = self.dataset1[idx]
-        m, s = np.mean(s1,axis=(0,1)), np.std(s1,axis=(0,1))
-
-        s1 = (s1 - m) / s # Normalize s1
-        x1 = torch.from_numpy(s1)
-            
-        return x1, self.x2[idx,:,:]/10000 
-    """
-
-
     def __getitem__(self, idx):
-        
+        """
+        Returns a single sample (pixel) from the dataset as a PyTorch tensor.
+        """
         if idx < self.dataset1.shape[0]:
             s1 = self.dataset1[idx]
-            m, s = np.mean(s1, axis=(0,1)), np.std(s1, axis=(0,1))
+            m, s = np.mean(s1,axis=(0,1)), np.std(s1,axis=(0,1))
             s1 = (s1 - m) / s # Normalize s1
-            x1 = torch.from_numpy(s1)
-            x2 = self.x2[idx // self.x2.shape[1], idx % self.x2.shape[1], :] / 10000
+            s1 = torch.from_numpy(s1)
+            s2 = self.dataset2[idx] / 10000 # Normalize s2
+            s2 = torch.from_numpy(s2)
         else:
             idx2 = idx - self.dataset1.shape[0]
-            x1 = torch.from_numpy(self.dataset1[-1])
-            x2 = self.x2[idx2 // self.x2.shape[1], idx2 % self.x2.shape[1], :] / 10000
+            s1 = torch.from_numpy(self.dataset1[-1])
+            s2 = self.dataset2[idx2] / 10000 # Normalize s2
+            s2 = torch.from_numpy(s2)
         
-        return x1, x2
+        s1 = s1.permute(1, 0)  # swap the T and D dimensions
+        s2 = s2.permute(1, 0)  # swap the T and D dimensions
+        
+        return s1, s2
