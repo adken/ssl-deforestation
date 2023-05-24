42125f829d84effb8a6343f0bb326fca279f0527
Kennedy Adriko <29119766+adken@users.noreply.github.com>
2023-05-24 11:31:49 +0200

    update train.py

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import torch
 from torchvision import datasets
 from torch.utils.data import DataLoader
-from utils.utils import augment, adjust_learning_rate, optim
-from models.model import VICRegNet
+from utils.utils import adjust_learning_rate, optim
+from models.vicreg import VICRegNet
 from utils.loss import sim_loss, cov_loss, std_loss
 from utils.dataset import TimeSeriesDataset
 import argparse
@@ -12,8 +12,8 @@ from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='VICReg Training')
 parser.add_argument('--path', help='path to datasets')
-parser.add_argument('--batch_size', default=16, type=int, help='batch size')
-parser.add_argument('--num_workers', type=int, default=4, help='number of workers')
+parser.add_argument('--num_workers', default=4, type=int, help='number of workers')
+parser.add_argument('--batch_size', default=1024, type=int, help='batch size')
 parser.add_argument('--device', default='cuda', type=str, help='device for training')
 parser.add_argument('--l', default=25,  help='coefficients of the invariance')
 parser.add_argument('--mu', default=25,  help='coefficients of the variance')
@@ -29,9 +29,12 @@ def main():
     print('Training Starts')
     args = parser.parse_args()
     writer = SummaryWriter(log_dir=args.log_dir)
-    dataset = TimeSeriesDataset(path=args.path)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size,drop_last=True, shuffle=False, num_workers=args.num_workers, prefetch_factor=2)
 
+    # load dataset
+    dataset = TimeSeriesDataset(path=args.path)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size,drop_last=True, shuffle=False,num_workers=args.num_workers, prefetch_factor=2) 
+
+    # create model
     model = VICRegNet().to(args.device)
     optimizer = optim(model, args.weight_decay)
 
@@ -42,8 +45,9 @@ def main():
         for step, ((view_a, view_b)) in loop:
             adjust_learning_rate(args, optimizer, dataloader, step)
             optimizer.zero_grad()
-            repr_a = model(view_a.to(args.device))
-            repr_b = model(view_b.to(args.device))
+            view_a = view_a.to(args.device, dtype=torch.float)
+            view_b = view_b.to(args.device, dtype=torch.float)
+            repr_a, repr_b = model(view_a, view_b)
 
             _sim_loss = sim_loss(repr_a, repr_b)
             _std_loss = std_loss(repr_a, repr_b)
@@ -53,15 +57,15 @@ def main():
             loss.backward()
             optimizer.step()
             writer.add_scalar("Loss/train", loss, epoch)
+            writer.add_scalar("Loss/sim", _sim_loss, epoch)
+            writer.add_scalar("Loss/std", _std_loss, epoch)
+            writer.add_scalar("Loss/cov", _cov_loss, epoch)
 
             if step % int(args.save_freq) == 0 and step:
                 with open(os.path.join(args.log_dir, 'logs.txt'), 'a') as log_file:
-                    log_file.write(f'Epoch: {epoch}, Step: {step}, Train loss: {loss.cpu().detach().numpy()} \n')
+                    log_file.write(f'Epoch: {epoch}, Step: {step}, Train loss: {loss.cpu().detach().numpy()}, Sim loss: {_sim_loss.cpu().detach().numpy()}, Std loss: {_std_loss.cpu().detach().numpy()}, Cov loss: {_cov_loss.cpu().detach().numpy()}\n')
 
-                state = dict(epoch=epoch + 1, model=model.state_dict(),
-                         optimizer=optimizer.state_dict())
 
-                torch.save(state, os.path.join('.', args.save_chpt, f'checkpoint_{step}_steps.pth'))
             loop.set_description(f'Epoch [{epoch}/{args.epoch}]')
             loop.set_postfix(loss = loss.cpu().detach().numpy())
 
@@ -75,4 +79,5 @@ def main():
 
 if '__main__' == __name__:
     main()
+    
 
