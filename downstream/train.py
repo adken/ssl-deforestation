2b83a6798a9fcd6c33afb70e6906db4fd5395ed2
Kennedy Adriko <29119766+adken@users.noreply.github.com>
2023-05-31 11:04:34 +0200

    update train.py

--- downstream/train.py
+++ downstream/train.py
@@ -15,7 +15,9 @@ import pandas as pd
 import os
 import sklearn.metrics
 import random
-
+import matplotlib.pyplot, numpy
+from sklearn.metrics import confusion_matrix 
+from sklearn.utils.multiclass import unique_labels
 
 def train(args):
         
@@ -214,6 +216,55 @@ def test_epoch(model, criterion, dataloader, device):
                 field_ids_list.append(field_id)
 
         return (torch.tensor(losses), torch.cat(y_true_list), torch.cat(y_pred_list), torch.cat(y_score_list), torch.cat(field_ids_list))
+    
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_confusion_matrix(y_true, y_pred, classes, normalize=False, title=None, cmap=None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = "Normalized Confusion Matrix"
+        else:
+            title = "Confusion Matrix"
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    # Plot confusion matrix
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes,
+           yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set alignment
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+
+    fig.tight_layout()
+    plt.show()
 
 
 def parse_args():
