00a5d4ef0f4b318aa9c1de1152fc946124fb19ff
Kennedy Adriko <29119766+adken@users.noreply.github.com>
2023-06-07 17:09:15 +0200

    update dataset.py

--- downstream/utils/dataset.py
+++ downstream/utils/dataset.py
@@ -3,33 +3,35 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+    
 
-
-class TimeSeriesDataset(Dataset):    
+class TimeSeriesDataset(Dataset):
     """
-    A PyTorch dataset class for loading satellite time series images from a NumPy file.
-    The images are expected to have dimensions N x T x C, where:
-    N: number of samples
+    A PyTorch dataset class for loading s1 and s2 satellite time series images from a NumPy file.
+    The images are expected to have dimensions N,T x C, where:
+    H: height of images
+    W: width of images
     T: number of timestamps in the time series
     C: number of channels in the images
     
     Parameters
     ----------
-    path : str
-        The path to the NumPy file containing the image data and labels.
-        
+    s1_path : str
+        The path to the directory containing the sentinel1 and sentinel2 times series data.
+      
     Attributes
     ----------
-    x : torch.Tensor
-        A tensor containing the image data loaded from the NumPy file.
+    s1, s2 : torch.Tensor
+        Tensors containing the image data loaded from the NumPy files.
     num_samples : int
         The total number of samples (pixels) in the dataset.
     """
+    
 
     def __init__(self, path, transforms=None):
         self.path = path
-        self.dataset1 = torch.from_numpy(np.load(os.path.join(self.path, 's1_data.npy'), mmap_mode='r'))
-        self.dataset2 = torch.from_numpy(np.load(os.path.join(self.path, 's2_data.npy'), mmap_mode='r'))
+        self.dataset1 = torch.from_numpy(np.load(os.path.join(self.path, 's1.npy'), mmap_mode='r+'))
+        self.dataset2 = torch.from_numpy(np.load(os.path.join(self.path, 's2.npy'), mmap_mode='r+'))
         self.labels = torch.from_numpy(np.load(os.path.join(self.path, 'labels.npy')))
         self.num_samples = self.dataset1.shape[0] or self.dataset2.shape[0]
         self.transforms = transforms
@@ -62,6 +64,4 @@ class TimeSeriesDataset(Dataset):
             s2 = self.transforms(s2)
 
         return s1, s2, label
-
-        
     
